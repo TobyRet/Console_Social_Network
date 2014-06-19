bc6c540e2420744dc30dde7a9e5723f37f7c6253
Toby Retallick <toby.retallick@gmail.com>
2014-06-19 17:53:58 +0100

    Added further scenario to publishing_message_spec

--- spec/publish_message_spec.rb
+++ spec/publish_message_spec.rb
@@ -4,13 +4,20 @@ require './lib/action'
 
 describe 'publishing a message' do
 
-  it 'can publish a message to a personal timeline' do
+  it 'can publish a message' do
 
     user = User.new('Alice')
     alice = Action.new(user)
 
     alice.post("I love the weather today")
     expect(alice.messages).to eq(["I love the weather today"])
+
+    user = User.new('Bob')
+    bob = Action.new(user)
+
+    bob.post("Damn! We lost!")
+    bob.post("Good game though.")
+    expect(bob.messages).to eq(["Damn! We lost!", "Good game though."])
 
   end
 
