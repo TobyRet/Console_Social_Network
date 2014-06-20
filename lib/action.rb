9a5e5a6095b812e4df8ba26a5454901062c4fbcd
Toby Retallick <toby.retallick@gmail.com>
2014-06-21 00:40:31 +0100

    Extracted timestamp into its own methof in action.rb

--- lib/action.rb
+++ lib/action.rb
@@ -8,8 +8,12 @@ class Action
   end
 
   def post(message)
-    @messages << { post: message, date: DateTime.now.strftime("%Y%m%dT%H%M%S") }
+    @messages << { post: message, date: post_date }
     puts @messages
+  end
+
+  def post_date
+    DateTime.now.strftime("%Y%m%dT%H%M%S")
   end
 
 end
