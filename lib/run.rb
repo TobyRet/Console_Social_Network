d63ef0133e242192ba029af5b5e40e3ef348fab9
Toby Retallick <toby.retallick@gmail.com>
2014-06-24 08:07:02 +0100

    User created when posting a message if it does not exist already

--- lib/run.rb
+++ lib/run.rb
@@ -3,66 +3,76 @@ require 'user'
 require 'timeline'
 require 'wall'
 
-user1 = User.new('Alice')
-user2 = User.new('Bob')
-user3 = User.new('Charlie')
+#user1 = User.new('Alice')
+#user2 = User.new('Bob')
+#user3 = User.new('Charlie')
+#
+#alice = Action.new(user1)
+#bob = Action.new(user2)
+#charlie = Action.new(user3)
+#
+#@users = [alice, bob, charlie]
 
-alice = Action.new(user1)
-bob = Action.new(user2)
-charlie = Action.new(user3)
+@all_users = []
 
-@users = [alice, bob, charlie]
+#puts "User profiles loaded: Alice, Bob & Charlie"
 
-puts "User profiles loaded: Alice, Bob & Charlie"
+puts 'Welcome to Chatter!'
+puts '-------------------'
 
 def process
   input = gets.chomp.split
   input[2] = input[2..-1].join(" ") if input.length > 3
 
-  if input.length == 1
-    read(input[0])
-  elsif input[1] == "->"
-    post_message(input[0], input[2])
+  if input[1] == "->"
+    user_name = "#{ input[0] }"
+    check_user(user_name)
+    post_message(@user_do, input[2])
+  elsif input.length == 1
+    check_user(user_name)
+    read(@user_do)
   elsif input[1] == "follows"
-    follow(input[0], input[2])
+    follow(user_action, input[2])
   elsif input[1] == "wall"
-    wall(input[0])
+    wall(user_action)
   else
     puts "Sorry I don't understand that command. Please check instructions in this application's README file"
   end
 
 end
 
-def read(name)
-  user = select_user(name)[0]
+def post_message(user, message)
+  user.post(message)
+  puts "* #{ user.user.name } has posted the following message: #{ message } *"
+end
+
+def read(user)
   user_messages = user.messages
+  puts user.messages.inspect
   user_timeline = Timeline.new(user, user_messages)
+  puts user_timeline.inspect
   user_timeline.view
 end
 
-def post_message(name, message)
-  user = select_user(name)[0]
-  user.post(message)
-  puts "* #{ name } has posted the following message: #{ message } *"
-end
-
-def follow(name, target)
-  user = select_user(name)[0]
+def follow(user, target)
   user.follow(target)
   puts "* #{ name } is now following #{ target } *"
 end
 
-def wall(name)
-  user = select_user(name)[0]
+def wall(user)
   user_wall = Wall.new(user)
-  puts user_wall.inspect
   user_wall.view
 end
 
-def select_user(name)
-  @users.select { |user| user if user.user.name == name }
+def check_user(username)
+  if !@all_users.include?(username)
+    @all_users << username
+    @user = User.new(username)
+    @user_do = Action.new(@user)
+  end
 end
 
 loop do
+  puts 'Please enter a command.'
   process
 end
