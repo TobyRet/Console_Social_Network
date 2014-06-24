a055201df9622b546ba169ebf2c2682dac1ac3d0
Toby Retallick <toby.retallick@gmail.com>
2014-06-24 08:59:06 +0100

    Can switch between instances of User

--- lib/run.rb
+++ lib/run.rb
@@ -3,19 +3,8 @@ require 'user'
 require 'timeline'
 require 'wall'
 
-#user1 = User.new('Alice')
-#user2 = User.new('Bob')
-#user3 = User.new('Charlie')
-#
-#alice = Action.new(user1)
-#bob = Action.new(user2)
-#charlie = Action.new(user3)
-#
-#@users = [alice, bob, charlie]
-
 @all_users = []
-
-#puts "User profiles loaded: Alice, Bob & Charlie"
+@active_users = []
 
 puts 'Welcome to Chatter!'
 puts '-------------------'
@@ -27,14 +16,19 @@ def process
   if input[1] == "->"
     user_name = "#{ input[0] }"
     check_user(user_name)
-    post_message(@user_do, input[2])
+    post_message(@current_user, input[2])
   elsif input.length == 1
+    user_name = "#{ input[0] }"
     check_user(user_name)
     read(@user_do)
   elsif input[1] == "follows"
-    follow(user_action, input[2])
+    user_name = "#{ input[0] }"
+    check_user(user_name)
+    follow(@user_do, input[2])
   elsif input[1] == "wall"
-    wall(user_action)
+    user_name = "#{ input[0] }"
+    check_user(user_name)
+    wall(@user_do)
   else
     puts "Sorry I don't understand that command. Please check instructions in this application's README file"
   end
@@ -48,9 +42,7 @@ end
 
 def read(user)
   user_messages = user.messages
-  puts user.messages.inspect
   user_timeline = Timeline.new(user, user_messages)
-  puts user_timeline.inspect
   user_timeline.view
 end
 
@@ -69,7 +61,15 @@ def check_user(username)
     @all_users << username
     @user = User.new(username)
     @user_do = Action.new(@user)
+    @active_users << @user_do
+    @current_user = select_current_user(username)
+  else
+    @current_user = select_current_user(username)
   end
+end
+
+def select_current_user(username)
+  @active_users.select { |active_user| active_user if active_user.user.name == username }[0]
 end
 
 loop do
