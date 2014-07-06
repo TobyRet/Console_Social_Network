5e683f1d197eee5d42779dfc311d603e757f3401
Toby Retallick <toby.retallick@gmail.com>
2014-07-06 18:33:42 +0100

    Fixed failing test on timeline - formatting error

--- lib/timeline.rb
+++ lib/timeline.rb
@@ -2,19 +2,24 @@ class Timeline
 
   def initialize(messages = [])
     @messages = messages
-    #@user = user
   end
 
   def view
     if @messages.empty?
       puts "This person has not posted any messages yet"
     else
-      @messages.map { |message| "#{ message[:post] } (#{ time_ago(message[:time]) } minutes ago)" }
+      @messages.map { |message| "#{ message[:post] } (#{ time_ago(message[:time]) })" }
     end
   end
 
   def time_ago(time_created)
-    ((Time.now - time_created) / 60).ceil
+    timestamp = Time.now - time_created
+    puts timestamp
+    if timestamp <= 60
+      "#{ timestamp } seconds ago"
+    else
+      "#{ (timestamp / 60).ceil } minutes ago"
+    end
   end
 
 end
--- spec/view_timeline_spec.rb
+++ spec/view_timeline_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require './lib/user'
 require './lib/timeline'
+require 'timecop'
 require_relative './helpers/users.rb'
 
 describe "User's timeline" do
@@ -9,14 +10,21 @@ describe "User's timeline" do
 
   it "it displays a user's published messages" do
 
+    Timecop.freeze Time.local(2014, 6, 30, 18, 0, 0)
     alice.post("I love the weather today")
+    Timecop.return
+
+    Timecop.freeze Time.local(2014, 6, 30, 18, 5, 0)
     alice.post("Where is the cheese?")
+    Timecop.return
+
+    Timecop.freeze Time.local(2014, 6, 30, 19, 0, 0)
     alice_messages = alice.messages
     alice_timeline = Timeline.new(alice_messages)
 
-    expect(alice_timeline.view).to eq(["I love the weather today (1 minutes ago)",
-                                       "Where is the cheese? (1 minutes ago)"])
 
+    expect(alice_timeline.view).to eq(["I love the weather today (60 minutes ago)","Where is the cheese? (55 minutes ago)"])
+    Timecop.return
   end
 
 end
